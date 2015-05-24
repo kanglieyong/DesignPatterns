433ebacb59c7e2f2ea1776a31793f6c9c6c0cd41
kanglieyong <kanglieyong@gmail.com>
2015-05-24 13:47:20 +0800

    fix some bugs

--- ThreadSafeLogger/Logger.h
+++ ThreadSafeLogger/Logger.h
@@ -1,21 +1,23 @@
-#ifndef LOGGER_H_
-#define LOGGER_H_
+#ifndef LOGGERH_H_
+#define LOGGERH_H_
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include <mutex>
 
 class Logger
 {
  public:
-  static Logger& instance();
-  
   static const std::string kLogLevelDebug;
-  static const std::string kLogLevelInfo;
   static const std::string kLogLevelError;
+  static const std::string kLogLevelInfo;
+
+  static Logger& instance();
 
   void log(const std::string& inMessage, const std::string& inLogLevel);
   void log(const std::vector<std::string>& inMessages, const std::string& inLogLevel);
-
+  
  protected:
   static Logger* pInstance;
   static const char* const kLogFileName;
@@ -28,15 +30,16 @@ class Logger
     ~Cleanup();
   };
 
-  void logHealper(const std::string& inMessage, const std::string& inLogLevel);
+  void logHelper(const std::string& inName, const std::string& inLogLevel);
   
  private:
   Logger();
   virtual ~Logger();
-  static std::mutex sMutex;
   
   Logger(const Logger& src);
-  Logger& operator(const Logger& rhs);
+  Logger& operator=(const Logger& rhs);
+
+  static std::mutex sMutex;
 };
 
 #endif
